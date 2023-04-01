3a873b8877109d8d2f5e98cdf3bea2f3138249c7
Woid Pointer <woidpointer@gmail.com>
2023-04-01 16:49:19 +0200

    removed not working format on save changes

--- init.lua
+++ init.lua
@@ -468,8 +468,7 @@ vim.keymap.set("n", "<leader>dl", vim.diagnostic.setloclist) -- [D]iagnostics [L
 
 -- LSP settings.
 --  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
-
+local on_attach = function(client, bufnr)
   -- NOTE: Remember that lua is a real programming language, and as such it is possible
   -- to define small helper and utility functions so you don't have to repeat yourself
   -- many times.
@@ -730,40 +729,12 @@ require("null-ls").setup({
   },
 })
 
-
--- [[ format on save ]]
-local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(client)
-            -- apply whatever logic you want (in this example, we'll only use null-ls)
-            return client.name == "null-ls"
-        end,
-        bufnr = bufnr,
-    })
-end
-
--- if you want to set up formatting on save, you can use this as a callback
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
--- add to your shared on_attach callback
-local on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                lsp_formatting(bufnr)
-            end,
-        })
-    end
-end
-
-
+-- [[ Keymaping]]
+--
+--
+-- Format current buffer
 vim.cmd('map <Leader>cf :lua vim.lsp.buf.format({ timeout_ms = 2000 })<CR>')
 
-
--- [[ Keymaping]]
 -- Reload init.lua
 vim.keymap.set("n", "<leader>so", ":luafile $MYVIMRC<CR>", { noremap = true, silent = true })
 
