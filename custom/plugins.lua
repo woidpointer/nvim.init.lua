c0f04e3fd9a2f9b92eb30d744f27320976a594ec
Woid Pointer <woidpointer@gmail.com>
2024-01-14 06:16:05 +0100

    Disabled annoying noice warnings

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -32,6 +32,12 @@ local plugins = {
             ["vim.lsp.util.stylize_markdown"] = true,
             ["cmp.entry.get_documentation"] = true,
           },
+          hover = {
+            enabled = false,
+          },
+          signature = {
+            enabled = false,
+          },
         },
         -- you can enable a preset for easier configuration
         presets = {
